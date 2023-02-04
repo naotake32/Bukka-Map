a5545df6354815956fd788d830f5b7181a58a68e
github naotake32 <take32.nao@gmail.com>
2023-02-03 19:50:22 -0800

    built schemas for user and pin

--- models/User.js
+++ models/User.js
@@ -7,5 +7,18 @@ const UserSchema = new mongoose.Schema({
         min: 3,
         max: 20,
         unique: true,
-    }
-});
+    },
+    email: {
+        type: String,
+        require: true,
+        max: 50,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required,
+        min: 8,
+    },
+},{ timestamps: true }
+);
+module.exports = mongoose.model("User",UserSchema);
--- server/index.js
+++ server/index.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-
 const app = express();
+const pinRoute = require("../routes/");
 
 dotenv.config();
+
+app.use(express.json())
 
 mongoose.connect(process.env.MONGO_URL);
 
 app.post("pins");
+
+app.use("/api/pins", pinRoute);
 
 app.listen(3001, () => {
     console.log("srver is running...");
