170d00a54670a2193de54e6ed0b09e0cedf58535
github naotake32 <take32.nao@gmail.com>
2023-02-16 01:06:26 -0800

    added marker and popup functions to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
-import Map, {Marker} from 'react-map-gl';
+import React from "react";
+import Map, {Marker, Popup} from 'react-map-gl';
+import "../src/App.css";
 
 function App() {
+  const [showPopup, setShowPopup] = React.useState(true);
   return (
     <>
       <Map
@@ -12,11 +15,32 @@ function App() {
       style={{width: 600, height: 400}}
       mapStyle="mapbox://styles/mapbox/streets-v9"
       mapboxAccessToken={import.meta.env.VITE_APP_MAPBOX}
-      />
-
+      >
+    {/* show icon */}
     <Marker longitude={-100} latitude={40} anchor="bottom" >
       <img src="./pin.png" />
     </Marker>
+
+    {/* show popup */}
+
+    {showPopup && (
+      <Popup longitude={-100} latitude={40}
+        anchor="bottom"
+        onClose={() => setShowPopup(false)}>
+        <div className='card'>
+          <label>Product Name</label>
+          <h4 className="product">Apple</h4>
+          <label>Price</label>
+          <p>2.5$</p>
+          <label>Description</label>
+          <p className="desc">this was a good deal!</p>
+          <span className="username">Posted by <b>Naoki</b></span>
+          <span className="date">3 hours ago</span>
+        </div>
+      </Popup>)}
+    </Map>
+
+
     </>
   );
 }
