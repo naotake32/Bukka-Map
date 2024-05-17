06b0897c9fee1daf3de463a6adf6c2f5c6532f0c
github naotake32 <take32.nao@gmail.com>
2024-05-16 21:37:04 -0700

    fixed index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors"); // CORSを追加
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const app = express();
@@ -7,21 +8,21 @@ const pinRoute = require("../routes/pins");
 
 dotenv.config();
 
-app.use(express.json())
+app.use(express.json());
+app.use(cors()); // CORSを有効にする
 
 mongoose.connect(process.env.MONGO_URL, () => console.log("DB connected"));
 
-
-app.get("/test", (req,res)=> res.json({msg:"testing...."}))
-app.use("/api/users",userRoute);
+app.get("/test", (req, res) => res.json({ msg: "testing...." }));
+app.use("/api/users", userRoute);
 app.use("/api/pins", pinRoute);
-app.post("pins");
 
-
-app.listen(3001, () => {
-    console.log("server is running...");
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
 });
-// module.exports = app;
+
+module.exports = app;
 // app.listen(`https://bukka-map-prototype.vercel.app/`, () => {
 //     console.log("server is running...");
 // });
