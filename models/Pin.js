4c5295b92484107f152b3bd4502f4c99c0843885
github naotake32 <take32.nao@gmail.com>
2024-05-28 18:59:43 -0700

    make users able to post without having usenames

--- models/Pin.js
+++ models/Pin.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const PinSchema = new mongoose.Schema({
     username: {
         type: String,
-        required: true,
+        required: false,
         min: 3
     },
     product: {
